5598749f9d34e4802a891dd7d64197d1247fc6b9
matheuspaivas <matheuspaivas@github.com>
2017-09-20 01:04:20 -0300

    Commit otimizado

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Simbolo parada = Lexico.getLex(0);		//Simbolos são os lexemas que são retornados.
+		Simbolo simbolo = Lexico.getLex(0);		//simbolobolos são os lexemas que são retornados.
 		Scanner s = new Scanner(System.in);		//Scanner para iterar sobre o retorno dos lexemas.
 	
 		int aux=0;
@@ -15,22 +15,22 @@ public class Main {
 		do {	//Enquanto não é o ultimo lexema ou nao houver erros na abertura do arquivo.
 			
 			System.out.println
-				("Lexema: "+parada.getLexema()+	//Imprime o Lexema.
-				 "\nToken: "+parada.getToken()+	//Imprime o Token.
-				 "\nTipo: "+parada.getTipo());	//Imprime o Tipo.
+				("Lexema: "+simbolo.getLexema()+	//Imprime o Lexema.
+				 "\nToken: "+simbolo.getToken()+	//Imprime o Token.
+				 "\nTipo: "+simbolo.getTipo());	//Imprime o Tipo.
 
 			s.nextLine();	//Ir para proximo.
 			
-			parada = Lexico.getLex(Lexico.pos);	//Procura o proximo Lexema.
+			simbolo = Lexico.getLex(Lexico.pos);	//Procura o proximo Lexema.
 			
-			if(parada.getToken().equals("EOF")) aux=1;
+			if(simbolo.getToken().equals("EOF")) aux=1;
 			
 		}while(aux==0);
 		
 		System.out.println
-		("Lexema: "+parada.getLexema()+	//Imprime o Lexema.
-		 "\nToken: "+parada.getToken()+	//Imprime o Token.
-		 "\nTipo: "+parada.getTipo());	//Imprime o Tipo.
+		("Lexema: "+simbolo.getLexema()+	//Imprime o Lexema.
+		 "\nToken: "+simbolo.getToken()+	//Imprime o Token.
+		 "\nTipo: "+simbolo.getTipo());	//Imprime o Tipo.
 		
 		s.close();	//Fechar Scanner.
 			
